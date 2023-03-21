0b56a013a30711d7b32d50614a961954293adacb
harunazakaria <Harunzy55@gmail.com>
2023-03-21 17:46:21 +0000

    add

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,8 +1,9 @@
-export default function getNeighborhoodsList() {
+
+rt default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
   const self = this;
-  this.addNeighborhood = function add(newNeighborhood) {
+  this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
     return self.sanFranciscoNeighborhoods;
   };
