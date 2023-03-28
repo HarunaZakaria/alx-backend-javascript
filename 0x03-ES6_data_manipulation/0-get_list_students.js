b82ab7cd4139b44eebb0a6c81dea9414f9ada569
harunaZakaria <Harunzy55@gmail.com>
2023-03-28 15:20:23 +0000

    add

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,20 +1,7 @@
-<<<<<<< HEAD
-/**
- * Retrieves a list of students.
- * @returns {{id: Number, firstName: String, location: String}[]}
- */
-export default function getListStudents() {
-    return [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ];
-  }
-=======
-const getListStudents = [
+function getListStudents() {
+  return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' }
-];
-console.log(getListStudents);
->>>>>>> b1a7f80a067fa81b7bf2a25d615eec43bda53e07
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  ];
+}
